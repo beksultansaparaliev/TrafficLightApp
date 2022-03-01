c16b6e73ce7d7f1ddd2de5e54916d80ef732501b
Beksultan <albinblack7@gmail.com>
2022-03-01 17:06:38 +0300

    Finish Traffic Light App

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redColor: UIView!
+    @IBOutlet weak var yellowColor: UIView!
+    @IBOutlet weak var greenColor: UIView!
+    @IBOutlet weak var changeTheLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redColor.layer.cornerRadius = 60
+        redColor.backgroundColor = .red
+        redColor.alpha = 0.3
+        
+        yellowColor.layer.cornerRadius = 60
+        yellowColor.backgroundColor = .yellow
+        yellowColor.alpha = 0.3
+        
+        greenColor.layer.cornerRadius = 60
+        greenColor.backgroundColor = .green
+        greenColor.alpha = 0.3
+        
+        changeTheLightButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func changingTheLight() {
+        
+        if redColor.alpha != 1 && yellowColor.alpha != 1 && greenColor.alpha != 1 {
+            changeTheLightButton.setTitle("NEXT", for: .normal)
+            redColor.alpha = 1
+        } else if redColor.alpha == 1 && yellowColor.alpha != 1 && greenColor.alpha != 1 {
+            yellowColor.alpha = 1
+            redColor.alpha = 0.3
+        } else if redColor.alpha != 1 && yellowColor.alpha == 1 && greenColor.alpha != 1 {
+            greenColor.alpha = 1
+            yellowColor.alpha = 0.3
+        } else if redColor.alpha != 1 && yellowColor.alpha != 1 && greenColor.alpha == 1 {
+            redColor.alpha = 1
+            greenColor.alpha = 0.3
+        }
+    }
 }
 
